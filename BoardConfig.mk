519aaed52288fcaeeb933ad68dbc238a8c832666
Shreesha Murthy <shreeshu10@gmail.com>
2016-09-05 12:57:11 +0000

    Define RIL version

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,8 @@ TARGET_CPU_CORTEX_A53 := true
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+BOARD_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_11 -DCOMPAT_SENSORS_M
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
