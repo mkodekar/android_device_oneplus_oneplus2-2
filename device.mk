8893576e8118032f6b37fa99e296f6c8b23f810c
Shreesha Murthy <shreeshu10@gmail.com>
2016-09-05 12:54:06 +0000

    op2: Enable telephony-ext

--- device.mk
+++ device.mk
@@ -237,6 +237,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8994
