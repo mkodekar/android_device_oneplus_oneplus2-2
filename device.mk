f32db464d6ded66a1704d8f9602b66ddaf496d7f
Ruben Brunk <rubenbrunk@google.com>
2016-09-06 14:44:39 +0000

    op2: Enable feature for VR ready devices.

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Vulkan Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
